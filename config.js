462a807cfc02f7011e514c142e7af5011bcc8f1b
Aqsa Ansari <aqsaansari.aquarian@gmail.com>
2025-07-22 01:36:46 +0500

    api url rectified

--- config.js
+++ config.js
@@ -8,4 +8,4 @@ export const ENV =
 export const API_BASE_URL =
   ENV === "dev"
     ? "http://localhost:5000"
-    : "https://5d246004-b1bf-465c-ae23-5042ca5144a9-00-3vjhokw1r7ke6.pike.replit.dev:5000/";
+    : "https://5d246004-b1bf-465c-ae23-5042ca5144a9-00-3vjhokw1r7ke6.pike.replit.dev:5000";
